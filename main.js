3f534d950a72cad733b696d6ae51a5e057bed669
Kazuhiro Kubota <k2.wanko@gmail.com>
2015-03-28 12:28:30 +0900

    Add LocationLooper to main.js

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 (function(){
   var milkcocoa = new MilkCocoa("https://io-ui7rsabaf.mlkcca.com");
+  var duration = 1000;
   //var message = milkcocoa.dataStore("message");
+  
+  var onPosition = function(pos) {
+  };
 
-  var geolocation = function (){
+  var onError = function(err) {
+    console.log("Error", err);
+  };
+
+  var geolocation = function() {
     if (!navigator.geolocation) {
       return;
     }
 
-    return new Promise(function(resolve, reject) {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+    navigator.geolocation.getCurrentPosition(onPosition, onError);
   };
 
-  geolocation()
-    .then(function(pos){
-      console.log(pos);
-    })
-    .catch(function(err){
-      console.error(err);
-    });
+  var locationLooper = function() {
+    
+    if (locationLooper.isCancel) return;
+
+    geolocation();
+    
+    setTimeout(LocationLooper, duration);
+  };
+  locationLooper.isCancel = false
+  locationLooper.cancel = function() {
+    locationLooper.isCancel = true;
+  }
+  setTimeout(LocationLooper, duration)
   
 })();
